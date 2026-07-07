baee9c51ca4590f6d103b66878786738387e012e
Ashutoshporwal-ashu <ashutoshporwal77@gmail.com>
2026-07-07 16:15:55 +0530

    add register controller

--- src/app.js
+++ src/app.js
@@ -16,4 +16,10 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+//routes import
+import userRouter from './routes/user.routes.js'
+
+// routes declear
+app.use("/api/v1/users", userRouter)
+
 export { app }
